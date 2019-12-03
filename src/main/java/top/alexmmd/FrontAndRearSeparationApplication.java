33c07f32d4d5326df25b837a8ba6cb33c6e4e8f4
wangyonghui <2930807240@qq.com>
2019-12-03 15:19:23 +0800

    Initial commit

---
+++ src/main/java/top/alexmmd/FrontAndRearSeparationApplication.java
@@ -0,0 +1,13 @@
+package top.alexmmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontAndRearSeparationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontAndRearSeparationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/alexmmd/FrontAndRearSeparationApplicationTests.java
@@ -0,0 +1,13 @@
+package top.alexmmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontAndRearSeparationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
